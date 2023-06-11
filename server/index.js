760ccaa9e32b0c7d0bdaad43d37226f14a85f734
Peter <peterhansendev@gmail.com>
2023-06-11 12:22:36 +0800

    cors

--- server/index.js
+++ server/index.js
@@ -1,27 +1,27 @@
 const express = require('express')
 const path = require('path')
 const transformData = require('./dataTransformer')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-  }
-  
-  app.use(cors(corsOptions))
+  origin: ['http://localhost:3000', 'https://nodeparamvalue.vercel.app']
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 app.post('/api', (req, res) => {
   const transformedData = transformData(req.body.input)
-  res.json({ message: transformedData }) 
-}) 
+  res.json({ message: transformedData })
+})
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
